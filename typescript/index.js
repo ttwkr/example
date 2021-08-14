089918fae1d21f4044a5c8be3b288eaed983e52a
ttwkr <ttwkor12@gmail.com>
2021-08-14 23:32:18 +0900

    delete index js

--- typescript/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var name = "Sangjin", age = 34, gender = "male";
-var sayHi = function (name, age, gender) {
-    console.log(name + " is " + age + " and " + gender);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
