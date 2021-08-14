f9d1a07b769321ceef96a4049b9e739a96a88f08
ttwkr <ttwkor12@gmail.com>
2021-08-14 23:31:38 +0900

    #1 normal function

---
+++ typescript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "Sangjin", age = 34, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log(name + " is " + age + " and " + gender);
+};
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
---
+++ typescript/index.ts
@@ -0,0 +1,11 @@
+const name = "Sangjin",
+    age = 34,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`${name} is ${age} and ${gender}`)
+}
+
+sayHi(name, age)
+
+export {}
