ac58bdd5636530526bea33f743ced1a2b0277d59
ttwkr <ttwkor12@gmail.com>
2021-08-15 21:36:40 +0900

    #3 interface

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,15 @@
+interface Human {
+    name : string,
+    age: number,
+    gender: string
+}
+
+const person = {
+    name : 'Me',
+    age: 11,
+    gender: 'female'
+}
+
 const name = "Sangjin",
     age = 34,
     gender = "male";
@@ -6,6 +18,12 @@ const sayHi = (name: string, age: number, gender?: string) => {
     console.log(`${name} is ${age} and ${gender}!`)
 }
 
+// use interface
+const sayBye = (person: Human) => {
+    console.log(`${person.name} is ${person.age} and ${person.gender}!!!`)
+}
+
 sayHi(name, age, gender)
+sayBye(person)
 
 export {}
