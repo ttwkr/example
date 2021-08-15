61b6ff7a15129c7513fc112242f7be1651df1273
ttwkr <ttwkor12@gmail.com>
2021-08-15 21:54:07 +0900

    #4 use class

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -4,11 +4,28 @@ interface Human {
     gender: string
 }
 
+//use class 
+class HumanClass {
+    public name: string
+    public age: number
+    public gender: string
+
+    constructor(name: string, age:number, gender:string){
+        this.name = name
+        this.age = age
+        this.gender = gender
+    }
+}
+
+// use interface
 const person = {
     name : 'Me',
     age: 11,
     gender: 'female'
 }
+
+//use class
+const S = new HumanClass('SSS', 126, 'male')
 
 const name = "Sangjin",
     age = 34,
@@ -23,7 +40,14 @@ const sayBye = (person: Human) => {
     console.log(`${person.name} is ${person.age} and ${person.gender}!!!`)
 }
 
+//use class
+const sayClass = (person: HumanClass) => {
+    console.log(`${person.name} is ${person.age} and ${person.gender}!!!`)
+}
+
 sayHi(name, age, gender)
 sayBye(person)
+sayClass(S)
+
 
 export {}
