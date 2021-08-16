243d544fada7c122842c4ea4384feb885ec152f3
ttwkr <ttwkor12@gmail.com>
2021-08-16 12:29:08 +0900

    #6 add block chain

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,9 +1,20 @@
+import * as Crypto  from 'crypto-js'
+
 class Block {
     public index:number
     public hash:string
     public previousHash:string
     public data:string
     public timestamp:number
+
+    //hash 암호화
+    static calculateHash = (
+        index:string, 
+        previousHash:string, 
+        data:string, 
+        timestamp:number
+        ):string => 
+        Crypto.SHA256(index+previousHash+data+timestamp).toString()
 
     constructor(index: number, hash:string, previousHash:string, data:string, timestamp:number){
         this.index = index
@@ -16,6 +27,10 @@ class Block {
 
 const genericBlock:Block = new Block(0, "20202020202", "", "hello", 123456)
 
-let blockChain:[Block] = [genericBlock]
+let blockChain:Block[] = [genericBlock]
 
+// 사용할 함수 설정
+const getBlockchain = ():Block[] => blockChain
+const getLastestBlock = ():Block => blockChain[blockChain.length - 1]
+const getNewTimeStamp = ():number => Math.round(new Date().getTime() / 1000)
 console.log(blockChain)
