331099fbab1b60bbf1a7936af95d6f3a21ea7f57
서상진 <seosangjin@seosangjin-ui-MacBookPro.local>
2022-04-05 23:11:55 +0900

    add go example

---
+++ go/variable/ex4.2/ex4.2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var minimunWage int = 10
+	var workingHour int = 20
+
+	var income int = minimunWage * workingHour
+
+	fmt.Println(minimunWage, workingHour, income)
+}
---
+++ go/variable/ex4.2/go.mod
@@ -0,0 +1,3 @@
+module ex4.2.go
+
+go 1.18
