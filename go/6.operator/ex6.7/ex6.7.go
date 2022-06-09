ed92d492f7f8d90e358bd8de39e49fd31bf5b19a
ttwkr <ttwkor12@gmail.com>
2022-06-09 23:58:20 +0900

    tes git auth

--- go/6.operator/ex6.7/ex6.7.go
+++ go/6.operator/ex6.7/ex6.7.go
@@ -25,7 +25,6 @@ func main() {
 	var a float64 = 0.1
 	var b float64 = 0.2
 	var c float64 = 0.3
-	var d float64 = 0.4
 
 	fmt.Printf("%0.18f + %0.18f = %0.18f\n", a, b, a+b)
 	fmt.Printf("%0.18f == %0.18f : %v\n", c, a+b, equal(a+b, c))
@@ -33,7 +32,6 @@ func main() {
 	a = 0.000000000004
 	b = 0.000000000002
 	c = 0.000000000007
-	d = 0.000000000008
 
 	fmt.Printf("%g == %g : %v\n", c, a+b, equal(a+b, c))
 }
