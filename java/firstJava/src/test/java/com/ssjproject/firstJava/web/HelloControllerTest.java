65202a15b990923495d9d0407698a66bf7c0752e
ttwkr <ttwkor12@gmail.com>
2022-07-17 11:03:58 +0900

    complete tuto java api

--- java/firstJava/src/test/java/com/ssjproject/firstJava/web/HelloControllerTest.java
+++ java/firstJava/src/test/java/com/ssjproject/firstJava/web/HelloControllerTest.java
@@ -1,9 +1,10 @@
 package com.ssjproject.firstJava.web;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -12,7 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
--- java/firstJava/src/test/java/com/ssjproject/firstJava/web/dto/HelloResponseDtoTest.java
+++ java/firstJava/src/test/java/com/ssjproject/firstJava/web/dto/HelloResponseDtoTest.java
@@ -1,5 +1,6 @@
 package com.ssjproject.firstJava.web.dto;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
