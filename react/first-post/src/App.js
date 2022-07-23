d3dc529022826392c07270e6dae8018e4c20ce99
ttwkr <ttwkor12@gmail.com>
2022-07-23 11:59:04 +0900

    add bootstrap for css

--- react/first-post/src/App.js
+++ react/first-post/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Titles from "./component/main/title"
 import {Link} from "react-router-dom";
+import {Button} from "react-bootstrap";
 
 const App = () => {
     return (
         <div className="App">
             <Titles/>
-            <Link to="post/save">글 등록</Link>
+            <Button variant="outline-primary">
+                <Link to="post/save" style={{ textDecoration: 'none' }}>글 등록</Link>
+            </Button>
+
         </div>
     );
 }
--- react/first-post/src/index.js
+++ react/first-post/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import {Routes, Route} from 'react-router'
 import {BrowserRouter} from "react-router-dom";
